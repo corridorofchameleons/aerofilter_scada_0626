08a9666cacfff5fd98f7b080354bd3bb02033ab3
CyberDemon <me@example.com>
2026-09-18 16:23:35 +0300

    update particles structure

--- app/instances/particles.py
+++ app/instances/particles.py
@@ -7,33 +7,31 @@ OIL_PREFIX = 'oil_particle_'
 FUEL_PREFIX = 'fuel_particle_'
 
 class OilParticles:
-    pass
+    ns_name = NS_NAME
 
 class FuelParticles:
-    pass
+    ns_name = NS_NAME
 
 
 def generate_oil_particle_data():
     oil_particles = OilParticles()
-    oil_particles.ns_name = NS_NAME
 
     for val in OIL_PARTICLES:
         setattr(
             oil_particles,
             f'{OIL_PREFIX}{val}um',
-            IntTag(oil_particles.ns_name)
+            IntTag(OilParticles.ns_name)
         )
     return oil_particles.__dict__
 
 def generate_fuel_particle_data():
     fuel_particles = OilParticles()
-    fuel_particles.ns_name = NS_NAME
 
     for val in OIL_PARTICLES:
         setattr(
             fuel_particles,
             f'{OIL_PREFIX}{val}um',
-            IntTag(fuel_particles.ns_name)
+            IntTag(FuelParticles.ns_name)
         )
     return fuel_particles.__dict__
 
